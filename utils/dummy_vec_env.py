900b96f09d53b6dbd64638fbd3e3ce4f40b5f446
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-12-07 12:58:42 +0800

    1. add done for all models. 2. add train_script4mujoco.py. 3. distinguish episodic task and continuing task in mpg learner: they have different value targets, but for model based learning, there is no different because MBRL cannot be properly used in episodic task. 4. rearrange mpg_learner.py and evaluator.py to suit for mujoco tasks.

--- utils/dummy_vec_env.py
+++ utils/dummy_vec_env.py
@@ -10,6 +10,14 @@ import numpy as np
 from gym.core import Wrapper
 
 
+def _get_state4inverted_double_pendulumv2(obs):
+    p, sintheta1, sintheta2, costheta1, costheta2, pdot, theta1dot, theta2dot \
+        = obs[0], obs[1], obs[2], obs[3], obs[4], obs[5], obs[6], obs[7]
+    theta1 = np.atan2(sintheta1, costheta1)
+    theta2 = np.atan2(sintheta2, costheta2)
+    return np.array([p, theta1, theta2, pdot, theta1dot, theta2dot])
+
+
 class DummyVecEnv(Wrapper):
     def __init__(self, env):
         super().__init__(env)
@@ -24,7 +32,9 @@ class DummyVecEnv(Wrapper):
     def reset(self, **kwargs):
         if 'init_obs' in kwargs.keys():
             init_obs = kwargs.get('init_obs')
-            self.env.reset(init_obs[0])
+            state = _get_state4inverted_double_pendulumv2(init_obs[0])
+            self.env.reset()
+            self.env.set_state(state[:3], state[3:])
             return init_obs
         else:
             if self.done:
