46489e50b950cf009fffaa65817fbedb718116df
guany17@mails.tsinghua.edu.cn <guany17@mail.tsinghua.edu.cn>
2020-12-09 20:33:30 +0800

    create inverted pendulum env for continuing task; change to continuing task

--- train_scripts/train_script4mujoco.py
+++ train_scripts/train_script4mujoco.py
@@ -107,7 +107,7 @@ def built_AMPC_parser():
     # tester and evaluator
     parser.add_argument('--num_eval_episode', type=int, default=5)
     parser.add_argument('--eval_log_interval', type=int, default=1)
-    parser.add_argument('--fixed_steps', type=int, default=None)
+    parser.add_argument('--fixed_steps', type=int, default=500)
     parser.add_argument('--eval_render', type=bool, default=True)
     parser.add_argument('--num_eval_agent', type=int, default=1)
 
@@ -241,7 +241,7 @@ def built_MPG_parser(version):
     # tester and evaluator
     parser.add_argument('--num_eval_episode', type=int, default=5)
     parser.add_argument('--eval_log_interval', type=int, default=1)
-    parser.add_argument('--fixed_steps', type=int, default=None)
+    parser.add_argument('--fixed_steps', type=int, default=500)
     parser.add_argument('--eval_render', type=bool, default=True)
     parser.add_argument('--num_eval_agent', type=int, default=1)
 
@@ -359,7 +359,7 @@ def built_NADP_parser():
     # tester and evaluator
     parser.add_argument('--num_eval_episode', type=int, default=5)
     parser.add_argument('--eval_log_interval', type=int, default=1)
-    parser.add_argument('--fixed_steps', type=int, default=None)
+    parser.add_argument('--fixed_steps', type=int, default=500)
     parser.add_argument('--eval_render', type=bool, default=True)
     parser.add_argument('--num_eval_agent', type=int, default=1)
 
