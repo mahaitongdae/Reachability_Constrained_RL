603feb17547467536c35528e9795700422bb8a3d
mahaitongdae <mahaitongdae@foxmail.com>
2021-01-04 20:45:21 +0800

    init commit for no v

--- learners/ampc.py
+++ learners/ampc.py
@@ -80,9 +80,6 @@ class AMPCLearner(object):
         veh2road4real_sum = self.tf.zeros((start_obses.shape[0],))
         obses = start_obses
         pf = self.punish_factor_schedule(ite)
-        processed_obses = self.preprocessor.tf_process_obses(obses)
-        obj_v_pred = self.policy_with_value.compute_obj_v(processed_obses)
-        con_v_pred = self.policy_with_value.compute_con_v(processed_obses)
 
         for _ in range(self.num_rollout_list_for_policy_update[0]):
             processed_obses = self.preprocessor.tf_process_obses(obses)
@@ -94,10 +91,6 @@ class AMPCLearner(object):
             veh2veh4real_sum += veh2veh4real
             veh2road4real_sum += veh2road4real
 
-        # obj v loss
-        obj_v_loss = self.tf.reduce_mean(self.tf.square(obj_v_pred - rewards_sum))
-        con_v_loss = self.tf.reduce_mean(self.tf.square(con_v_pred - real_punish_terms_sum))
-
         # pg loss
         obj_loss = -self.tf.reduce_mean(rewards_sum)
         punish_term_for_training = self.tf.reduce_mean(punish_terms_for_training_sum)
@@ -108,24 +101,20 @@ class AMPCLearner(object):
         veh2veh4real = self.tf.reduce_mean(veh2veh4real_sum)
         veh2road4real = self.tf.reduce_mean(veh2road4real_sum)
 
-        return obj_v_loss, con_v_loss, obj_loss, punish_term_for_training, punish_loss, pg_loss,\
+        return obj_loss, punish_term_for_training, punish_loss, pg_loss,\
                real_punish_term, veh2veh4real, veh2road4real, pf
 
     @tf.function
     def forward_and_backward(self, mb_obs, ite, mb_ref_index):
         with self.tf.GradientTape(persistent=True) as tape:
-            obj_v_loss, con_v_loss, obj_loss, punish_term_for_training, punish_loss, pg_loss, \
+            obj_loss, punish_term_for_training, punish_loss, pg_loss, \
             real_punish_term, veh2veh4real, veh2road4real, pf\
                 = self.model_rollout_for_update(mb_obs, ite, mb_ref_index)
 
         with self.tf.name_scope('policy_gradient') as scope:
             pg_grad = tape.gradient(pg_loss, self.policy_with_value.policy.trainable_weights)
-        with self.tf.name_scope('obj_v_gradient') as scope:
-            obj_v_grad = tape.gradient(obj_v_loss, self.policy_with_value.obj_v.trainable_weights)
-        with self.tf.name_scope('con_v_gradient') as scope:
-            con_v_grad = tape.gradient(con_v_loss, self.policy_with_value.con_v.trainable_weights)
 
-        return pg_grad, obj_v_grad, con_v_grad, obj_v_loss, con_v_loss, obj_loss, \
+        return pg_grad, obj_loss, \
                punish_term_for_training, punish_loss, pg_loss,\
                real_punish_term, veh2veh4real, veh2road4real, pf
 
@@ -144,14 +133,12 @@ class AMPCLearner(object):
         mb_ref_index = self.tf.constant(self.batch_data['batch_ref_index'], self.tf.int32)
 
         with self.grad_timer:
-            pg_grad, obj_v_grad, con_v_grad, obj_v_loss, con_v_loss, obj_loss, \
+            pg_grad, obj_loss, \
             punish_term_for_training, punish_loss, pg_loss, \
             real_punish_term, veh2veh4real, veh2road4real, pf =\
                 self.forward_and_backward(mb_obs, iteration, mb_ref_index)
 
             pg_grad, pg_grad_norm = self.tf.clip_by_global_norm(pg_grad, self.args.gradient_clip_norm)
-            obj_v_grad, obj_v_grad_norm = self.tf.clip_by_global_norm(obj_v_grad, self.args.gradient_clip_norm)
-            con_v_grad, con_v_grad_norm = self.tf.clip_by_global_norm(con_v_grad, self.args.gradient_clip_norm)
 
         self.stats.update(dict(
             iteration=iteration,
@@ -163,15 +150,11 @@ class AMPCLearner(object):
             veh2road4real=veh2road4real.numpy(),
             punish_loss=punish_loss.numpy(),
             pg_loss=pg_loss.numpy(),
-            obj_v_loss=obj_v_loss.numpy(),
-            con_v_loss=con_v_loss.numpy(),
             punish_factor=pf.numpy(),
             pg_grads_norm=pg_grad_norm.numpy(),
-            obj_v_grad_norm=obj_v_grad_norm.numpy(),
-            con_v_grad_norm=con_v_grad_norm.numpy()
         ))
 
-        grads = obj_v_grad + con_v_grad + pg_grad
+        grads = pg_grad
 
         return list(map(lambda x: x.numpy(), grads))
 
--- policy.py
+++ policy.py
@@ -29,24 +29,14 @@ class Policy4Toyota(tf.Module):
         self.args = args
         obs_dim, act_dim = self.args.obs_dim, self.args.act_dim
         n_hiddens, n_units, hidden_activation = self.args.num_hidden_layers, self.args.num_hidden_units, self.args.hidden_activation
-        value_model_cls, policy_model_cls = NAME2MODELCLS[self.args.value_model_cls], \
-                                            NAME2MODELCLS[self.args.policy_model_cls]
+        policy_model_cls = NAME2MODELCLS[self.args.policy_model_cls]
         self.policy = policy_model_cls(obs_dim, n_hiddens, n_units, hidden_activation, act_dim * 2, name='policy',
                                        output_activation=self.args.policy_out_activation)
         policy_lr_schedule = PolynomialDecay(*self.args.policy_lr_schedule)
         self.policy_optimizer = self.tf.keras.optimizers.Adam(policy_lr_schedule, name='adam_opt')
 
-        self.obj_v = value_model_cls(obs_dim, n_hiddens, n_units, hidden_activation, 1, name='obj_v')
-        self.con_v = value_model_cls(obs_dim, n_hiddens, n_units, hidden_activation, 1, name='con_v')
-
-        obj_value_lr_schedule = PolynomialDecay(*self.args.value_lr_schedule)
-        self.obj_value_optimizer = self.tf.keras.optimizers.Adam(obj_value_lr_schedule, name='objv_adam_opt')
-
-        con_value_lr_schedule = PolynomialDecay(*self.args.value_lr_schedule)
-        self.con_value_optimizer = self.tf.keras.optimizers.Adam(con_value_lr_schedule, name='conv_adam_opt')
-
-        self.models = (self.obj_v, self.con_v, self.policy,)
-        self.optimizers = (self.obj_value_optimizer, self.con_value_optimizer, self.policy_optimizer)
+        self.models = (self.policy,)
+        self.optimizers = (self.policy_optimizer,)
 
     def save_weights(self, save_dir, iteration):
         model_pairs = [(model.name, model) for model in self.models]
@@ -69,33 +59,13 @@ class Policy4Toyota(tf.Module):
 
     @tf.function
     def apply_gradients(self, iteration, grads):
-        obj_v_len = len(self.obj_v.trainable_weights)
-        con_v_len = len(self.con_v.trainable_weights)
-        obj_v_grad, con_v_grad, policy_grad = grads[:obj_v_len], \
-                                            grads[obj_v_len:obj_v_len+con_v_len], \
-                                            grads[obj_v_len+con_v_len:]
-        self.obj_value_optimizer.apply_gradients(zip(obj_v_grad, self.obj_v.trainable_weights))
-        self.con_value_optimizer.apply_gradients(zip(con_v_grad, self.con_v.trainable_weights))
-        self.policy_optimizer.apply_gradients(zip(policy_grad, self.policy.trainable_weights))
+        self.policy_optimizer.apply_gradients(zip(grads, self.policy.trainable_weights))
 
     @tf.function
     def compute_mode(self, obs):
         logits = self.policy(obs)
         mean, _ = self.tf.split(logits, num_or_size_splits=2, axis=-1)
         return self.args.action_range * self.tf.tanh(mean) if self.args.action_range is not None else mean
-
-    def _logits2dist(self, logits):
-        mean, log_std = self.tf.split(logits, num_or_size_splits=2, axis=-1)
-        act_dist = self.tfd.MultivariateNormalDiag(mean, self.tf.exp(log_std))
-        if self.args.action_range is not None:
-            act_dist = (
-                self.tfp.distributions.TransformedDistribution(
-                    distribution=act_dist,
-                    bijector=self.tfb.Chain(
-                        [self.tfb.Affine(scale_identity_multiplier=self.args.action_range),
-                         self.tfb.Tanh()])
-                ))
-        return act_dist
 
     @tf.function
     def compute_action(self, obs):
@@ -104,21 +74,6 @@ class Policy4Toyota(tf.Module):
             if self.args.deterministic_policy:
                 mean, log_std = self.tf.split(logits, num_or_size_splits=2, axis=-1)
                 return self.args.action_range * self.tf.tanh(mean) if self.args.action_range is not None else mean, 0.
-            else:
-                act_dist = self._logits2dist(logits)
-                actions = act_dist.sample()
-                logps = act_dist.log_prob(actions)
-                return actions, logps
-
-    @tf.function
-    def compute_obj_v(self, obs):
-        with self.tf.name_scope('compute_obj_v') as scope:
-            return tf.squeeze(self.obj_v(obs), axis=1)
-
-    @tf.function
-    def compute_con_v(self, obs):
-        with self.tf.name_scope('compute_con_v') as scope:
-            return tf.squeeze(self.con_v(obs), axis=1)
 
 
 def test_policy():
