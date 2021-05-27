9c08e73f79b0a9e3953cb7732fd8f08ae4d2654a
mahaitong <mahaitongdae@foxmail.com>
2021-05-27 20:37:14 +0800

    update over cost lim and init bias

--- evaluator.py
+++ evaluator.py
@@ -490,7 +490,7 @@ class EvaluatorWithCost(object):
         if self.eval_times % self.args.eval_log_interval == 0:
             logger.info('Evaluator_info: {}, {}'.format(self.get_stats(), mean_metric_dict))
         self.eval_times += 1
-        over_cost_lim = mean_metric_dict['episode_cost'] > self.args.cost_lim * 10 * 1.1 # todo
+        over_cost_lim = mean_metric_dict['episode_cost'] > self.args.cost_lim * 10 * 1.2 # todo
         return over_cost_lim
 
     def run_evaluation_demo(self, iteration):
