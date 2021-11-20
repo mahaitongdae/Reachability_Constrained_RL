2ee5fb26af61a4b40fde90fe2182bf23262793db
mahaitong <mahaitongdae@foxmail.com>
2021-11-20 23:40:49 +0800

    update 1120

--- static_region.py
+++ static_region.py
@@ -1,6 +1,41 @@
 from policy import Policy4Lagrange
 import os
 from evaluator import Evaluator
+import gym
+from utils.em_brake_4test import EmergencyBraking
+import numpy as np
+from matplotlib.colors import ListedColormap
+from gym.envs.user_defined.EmerBrake.models import EmBrakeModel
+
+
+def model_rollout_for_update(self, start_obses, ite):
+    start_obses = self.tf.tile(start_obses, [self.M, 1])
+    self.model.reset(start_obses)
+    rewards_sum = self.tf.zeros((start_obses.shape[0],))
+    obses = start_obses
+    constraints_list = []
+    for step in range(self.num_rollout_list_for_policy_update[0]):
+        processed_obses = self.preprocessor.tf_process_obses(obses)
+        actions, _ = self.policy_with_value.compute_action(processed_obses)
+        obses, rewards, constraints = self.model.rollout_out(actions)
+        constraints_clip = self.tf.clip_by_value(constraints, CONSTRAINTS_CLIP_MINUS, 100)
+        constraints_list.append(self.tf.expand_dims(constraints_clip, axis=1))
+
+        rewards_sum += self.preprocessor.tf_process_rewards(rewards)
+    constraints_all = self.tf.concat(constraints_list, 1)
+    processed_start_obses = self.preprocessor.tf_process_obses(start_obses)
+    mu_all = self.policy_with_value.compute_mu(processed_start_obses)
+    cs_sum = self.tf.reduce_sum(self.tf.multiply(mu_all, self.tf.stop_gradient(constraints_all)), 1)
+    punish_terms_sum = self.tf.reduce_sum(self.tf.multiply(self.tf.stop_gradient(mu_all), constraints_all), 1)
+
+    obj_loss = -self.tf.reduce_mean(rewards_sum)
+    punish_terms = self.tf.reduce_mean(punish_terms_sum)
+    pg_loss = obj_loss + punish_terms
+    cs_loss = -self.tf.reduce_mean(cs_sum)
+    constraints = self.tf.reduce_mean(constraints_all)
+
+    return obj_loss, punish_terms, cs_loss, pg_loss, constraints
+
 
 
 def static_region(test_dir, iteration):
@@ -22,7 +57,62 @@ def static_region(test_dir, iteration):
     args =  parser.parse_args()
     evaluator = Evaluator(Policy4Lagrange, args.env_id, args)
     evaluator.load_weights(os.path.join(test_dir, 'models'), iteration)
-    evaluator.static_region()
+    model = EmBrakeModel()
+
+    # generate batch obses
+    d = np.linspace(0, 10, 100)
+    v = np.linspace(0, 10, 100)
+    # cmaplist = ['springgreen'] * 3 + ['crimson'] * 87
+    # cmap1 = ListedColormap(cmaplist)
+    D, V = np.meshgrid(d, v)
+    flatten_d = np.reshape(D, [-1, ])
+    flatten_v = np.reshape(V, [-1, ])
+    init_obses = np.stack([flatten_d, flatten_v], 1)
+
+    # define rollout
+    def reduced_model_rollout_for_update(obses):
+        model.reset(obses)
+        constraints_list = []
+        for step in range(args.num_rollout_list_for_policy_update[0]):
+            processed_obses = evaluator.preprocessor.tf_process_obses(obses)
+            actions, _ = evaluator.policy_with_value.compute_action(processed_obses)
+            obses, rewards, constraints = model.rollout_out(actions)
+            constraints_list.append(constraints)
+        flattern_cstr = evaluator.tf.stack(constraints_list, 1).numpy()
+        return flattern_cstr
+    flatten_cstr = reduced_model_rollout_for_update(init_obses)
+
+    preprocess_obs = evaluator.preprocessor.np_process_obses(init_obses)
+    flatten_mu = evaluator.policy_with_value.compute_mu(preprocess_obs).numpy()
+
+    flatten_cs = np.multiply(flatten_cstr, flatten_mu)
+
+    def plot_region(data, name):
+        for k in range(data.shape[1]):
+            data_k = data[:, k]
+            data_reshape = data_k.reshape(D.shape)
+            import matplotlib.pyplot as plt
+            from mpl_toolkits.mplot3d import Axes3D
+            plt.figure()
+            plt.grid()
+            x = np.linspace(0, 10)
+            t = np.sqrt(2 * 5 * x)
+            ct = plt.contour(D, V, data_reshape, 50, cmap='rainbow')
+            plt.colorbar(ct)
+            plt.plot(x, t, linestyle='--', color='red')
+            name_2d = name + '_' + str(k) + '_2d.jpg'
+            plt.savefig(os.path.join(evaluator.log_dir, name_2d))
+
+            figure = plt.figure()
+            ax = Axes3D(figure)
+            ax.plot_surface(D, V, data_reshape, rstride=1, cstride=1, cmap='rainbow')
+            # plt.show()
+            name_3d = name + '_' + str(k) + '_3d.jpg'
+            plt.savefig(os.path.join(evaluator.log_dir, name_3d))
+
+    plot_region(flatten_cs, 'cs')
+
+
 
 if __name__ == '__main__':
     static_region('./results/toyota3lane/LMAMPC-v2-2021-06-24-14-10-01', 200000)
---
+++ utils/em_brake_4test.py
@@ -0,0 +1,114 @@
+import gym
+import numpy as np
+from gym.utils import seeding
+import matplotlib.pyplot as plt
+
+class EmergencyBraking(gym.Env):
+    def __init__(self, **kwargs):
+        metadata = {'render.modes': ['human']}
+        self.step_length = 0.1  # ms
+        self.action_number = 1
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(self.action_number,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(np.full([2,], -float('inf')),np.full([2,], float('inf')), dtype=np.float32)
+        self.obs = self._reset_init_state()
+        self.A = np.array([[1, -self.step_length],[0,1]])
+        self.B = np.array([[0],[self.step_length]])
+
+
+    def reset(self, reset_obs=None):
+        '''
+        ndarray([2,])
+        '''
+        if reset_obs is None:
+            self.obs = self._reset_init_state()
+        else:
+            self.obs = reset_obs
+        self.action = np.array([0.0])
+        self.cstr = 0.0
+        return self.obs
+
+    def step(self, action):
+        if len(action.shape) == 2:
+            action = action.reshape([-1,])
+        self.action = self._action_transform(action)
+        reward = self.compute_reward(self.obs, self.action)
+        self.obs = (np.matmul(self.A, self.obs[:, np.newaxis]) + np.matmul(self.B, self.action[:, np.newaxis])).reshape([-1,])
+        if self.obs[1] < 0: self.obs[1] = 0
+        done = True if self.obs[0] < 0 or self.obs[1] <= 0 else False
+        # constraint = np.where(self.obs[0]<0, -self.obs[0], np.zeros_like(self.obs[0])) # todo: problem
+        constraint = -self.obs[0]
+        info = dict(reward_info=dict(reward=reward, constraints=float(constraint)))
+        self.cstr = constraint
+        return self.obs, reward, done, info # s' r 
+
+
+
+    def _action_transform(self, action):
+        action = 5 * np.clip(action, -1.05, 1.05)
+        return action
+
+    def compute_reward(self, obs, action):
+        # r = -0.01 * (action[0] ** 2) # obs[0] ** 2 + obs[1] ** 2 +
+        r = -0.01 * np.square(obs[1] - 5.0)
+        return r
+
+    def _reset_init_state(self):
+        d = 10 * np.random.random()
+        v = np.sqrt(2 * 5 * d) * np.random.random()
+        # v = 10 * np.random.random()
+        return np.array([d,v])
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def render(self, mode='human'):
+        extension = 1
+        if mode == 'human':
+            plt.ion()
+            plt.cla()
+            plt.title("Emergency Braking")
+            ax = plt.axes(xlim=(-7 - extension, 5 + extension),
+                          ylim=(-1, 5))
+            plt.axis("equal")
+            plt.axis('off')
+
+            plt.plot([-7, 5], [0, 0])
+            plt.plot([5, 5], [0, 3])
+            ax.add_patch(plt.Rectangle((-self.obs[0]+5-2, 0), 2, 1, edgecolor='black', facecolor='none'))
+
+            text_x, text_y = -5, 5
+            plt.text(text_x, text_y, 'distance: {:.2f}m'.format(self.obs[0]))
+            plt.text(text_x, text_y - 1, 'velocity: {:.2f}m'.format(self.obs[1]))
+            plt.text(text_x, text_y - 2, 'action: {:.2f}m'.format(self.action[0]))
+            plt.text(text_x, text_y - 3, 'constraints: {:.2f}m'.format(self.cstr))
+            plt.show()
+            plt.pause(0.001)
+
+def test_env():
+    import time
+    env = EmergencyBraking()
+    env.step_length = 0.01
+    obs = env.reset()
+    i = 0
+    done = 0
+    action = np.array([-1])
+    while True:
+        obs, reward, done, info = env.step(action)
+        env.render()
+        time.sleep(0.5)
+        if obs[1]<=0.1: env.reset()
+        print(reward)
+
+if __name__ == '__main__':
+    test_env()
+
+
+
+
+
+
+
+
+
+
